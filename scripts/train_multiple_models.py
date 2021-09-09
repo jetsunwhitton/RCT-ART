b64aa244383ac551ee0d4c7b1e457f68d06230ee
Jetsun Whitton <jetsun.whitton@gmail.com>
2021-09-09 16:21:45 +0100

    "hyper_param testing"

--- scripts/train_multiple_models.py
+++ scripts/train_multiple_models.py
@@ -25,7 +25,7 @@ def train_across_strats(file_dir, config, model_type):
         name = "strat_" + re.search("\d+\%",strat).group(0)
         print(name)
         os.system(f"python -m spacy train {config} " \
-                  f"--output ../trained_models/{model_type}/all_domain_strats/{name} " \
+                  f"--output ../trained_models/bio_bert/{model_type}/all_domain_strats/{name} " \
                   f"--paths.train ../datasets/preprocessed/all_domains/training_stratifications/{strat} " \
                   f"--paths.dev ../datasets/preprocessed/all_domains/results_only/dev.spacy " \
                   f"-c ../scripts/custom_functions.py --gpu-id 0")
@@ -39,7 +39,11 @@ if __name__ == "__main__":
     #train_across_strats("../datasets/preprocessed/all_domains/training_stratifications", "../configs/ner_biobert.cfg", "ner")
 
     # rel
-    train_across_strats("../datasets/preprocessed/all_domains/training_stratifications", "../configs/rel_biobert.cfg", "rel")
+    train_across_strats("../datasets/preprocessed/all_domains/training_stratifications", "../configs/ner_biobert.cfg",
+                        "ner")
+
+    train_across_strats("../datasets/preprocessed/all_domains/training_stratifications", "../configs/ner_biobert.cfg",
+                        "rel")
 
 
 #def train_across_strats():
